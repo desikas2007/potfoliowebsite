84805ef501709dd1d3ef3b478056a7cd2c81ff56
desikas2007 <desikas209@gmail.com>
2025-11-04 12:18:41 +0530

    Contact.js

---
+++ Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section className="contact-page" data-aos="fade-up">
+      <h2>Contact Me</h2>
+      <p>Email: example@gmail.com</p>
+      <p>LinkedIn: linkedin.com/in/example</p>
+    </section>
+  );
+}
+
+export default Contact;
