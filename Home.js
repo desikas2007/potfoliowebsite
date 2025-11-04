64b6dd683cfd45af466bc1bfb5ad6d6ee7a7d491
desikas2007 <desikas209@gmail.com>
2025-11-04 05:31:23 +0530

    Home.js

---
+++ Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <section id="home">
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is your existing homepage content.</p>
+      <img src="profilephoto.jpeg" alt="My Profile" style={{ width: "200px", borderRadius: "50%" }} />
+    </section>
+  );
+}
