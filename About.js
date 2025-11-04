a7ec5e6258ee7d449ef3611ab08dd45bb2a95a4b
desikas2007 <desikas209@gmail.com>
2025-11-04 12:17:28 +0530

    About.js

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="about-page" data-aos="fade-up">
+      <h2>About Me</h2>
+      <p>I’m a passionate web developer skilled in React, JavaScript, and modern web design.</p>
+    </section>
+  );
+}
+
+export default About;
