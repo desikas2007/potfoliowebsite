926fdbf8e380a174f210b58f5fd9858aeb1eff0f
desikas2007 <desikas209@gmail.com>
2025-11-05 15:10:16 +0530

    App.js

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./pages/Navbar";
-import Home from "./components/pages/Home";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 import About from "./pages/About";
+import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
@@ -18,4 +22,3 @@ function App() {
 }
 
 export default App;
-
