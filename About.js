f7185b649b7844d57383f8c9e577831d142bc496
desikas2007 <desikas209@gmail.com>
2025-11-05 15:13:20 +0530

    About.js

--- About.js
+++ About.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
+import "./About.css";
 
-function About() {
+export default function About() {
   return (
-    <section className="about-page" data-aos="fade-up">
+    <section className="about container" data-aos="fade-up">
       <h2>About Me</h2>
-      <p>I’m a passionate web developer skilled in React, JavaScript, and modern web design.</p>
+      <p>
+        Hi, I'm <strong>Desika S</strong> — a passionate Full Stack Developer who loves 
+        building modern and user-friendly web applications. My focus is on 
+        React, Firebase, and UI/UX design and a budding computer science and engineering pursuing my BE in CSE at KSRCE. When I'm not immersed in academics, you can find me creating sharing my knowledge and experiences with the world. I'm driven by a desire to harness technology for social impact, and I enjoy working on innovative mini-projects that can make a real difference in people's lives. I'm excited to share my journey, projects, and insights with you through this portfolio.
+      </p>
     </section>
   );
 }
-
-export default About;
