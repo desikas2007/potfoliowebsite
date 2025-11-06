20f2c04af0f0aa19936e9720f2048f69f0ff9fcd
desikas2007 <desikas209@gmail.com>
2025-11-07 05:10:30 +0530

    Header.js

--- Header.js
+++ Header.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
-function Header() {
+export default function Header() {
   return (
     <header className="header">
-      <h1 className="logo">My Portfolio</h1>
-      <nav className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+      {/* Left side: Name/Logo */}
+      <div className="logo">
+        <Link to="/">DESIKA S</Link>
+      </div>
+
+      {/* Right side: Navigation menu */}
+      <nav className="navbar">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
       </nav>
     </header>
   );
 }
-
-export default Header;
