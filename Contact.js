555c14a17e845bc4031eb7e2fb58aa64e337e083
desikas2007 <desikas209@gmail.com>
2025-11-15 12:51:10 +0530

    Contact.js

--- Contact.js
+++ Contact.js
@@ -5,61 +5,45 @@ export default function Contact() {
   return (
     <section className="contact-section">
 
-      {/* Background */}
-      <div className="background-flower"></div>
-
-      {/* Big left vertical text */}
-      <div className="big-contact">PROFILE</div>
-
       <div className="contact-content">
-
         <h1 className="main-title">CONTACT</h1>
 
         <p className="small-text">
           Please contact me at this e-mail address to discuss your site creation needs.
         </p>
 
-        {/* CLICKABLE EMAIL → GOOGLE FORM */}
-        <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSew-QhFrSl-aIQDD5D4G4xrUn198eDMv_yEs5t-11l_kYk59Q/viewform?usp=publish-editor"
-          target="_blank"
-          rel="noreferrer"
-          className="email-text email-link"
+        {/* Click email → open Google Form */}
+        <h2
+          className="email-text"
+          onClick={() =>
+            window.open(
+              "https://docs.google.com/forms/d/e/1FAIpQLSew-QhFrSl-aIQDD5D4G4xrUn198eDMv_yEs5t-11l_kYk59Q/viewform?usp=publish-editor",
+              "_blank"
+            )
+          }
         >
           desikas2007@gmail.com
-        </a>
+        </h2>
 
-        <p className="get-in-touch">Get in touch</p>
-
-        {/* SOCIAL ICONS */}
+        {/* Icons */}
         <footer className="social-footer">
-
-          <a
-            href="https://www.linkedin.com/in/desika-s-370b55363"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://www.linkedin.com/in/desika-s-370b55363?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">
             <i className="fa-brands fa-linkedin"></i>
           </a>
 
-          <a
-            href="https://github.com/desikas2007"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://github.com/desikas2007" target="_blank">
             <i className="fa-brands fa-github"></i>
           </a>
 
-          <a
-            href="https://www.instagram.com/_itz_desika_s/"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://www.instagram.com/_itz_desika_s/" target="_blank">
             <i className="fa-brands fa-instagram"></i>
           </a>
-
         </footer>
 
+        {/* COPYRIGHT */}
+        <p className="copyright">
+          © 2025 Desika | All Rights Reserved
+        </p>
       </div>
     </section>
   );
