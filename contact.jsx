e3ad4fbe3d3c86b56d1c62b505d5b15da7f89643
desikas2007 <desikas209@gmail.com>
2025-11-25 11:45:05 +0530

    contact.jsx

--- contact.jsx
+++ contact.jsx
@@ -1,50 +1,44 @@
 import React from "react";
-import "./Contact.css";
+import "../styles/theme.css";
+import "../styles/Contact.css";
+import { Link } from "react-router-dom";
+
+// Left-side image
+import contactImg from "../assets/contact-image.jpeg";
 
 export default function Contact() {
+  const googleFormLink = "https://docs.google.com/forms/d/e/1FAIpQLSew-QhFrSl-aIQDD5D4G4xrUn198eDMv_yEs5t-11l_kYk59Q/viewform?usp=publish-editor";
+
   return (
-    <section className="contact-section">
+    <div className="contact-page">
+      {/* Header */}
+      <header className="header-bar">
+        <h2 className="logo">DESIKA PORTFOLIO</h2>
+        <nav className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+      </header>
 
-      <div className="contact-content">
-        <h1 className="main-title">GET IN TOUCH</h1>
-
-        <p className="small-text">
-          Please contact me at this e-mail address to discuss your site creation needs.
-        </p>
-
-        {/* Click email → open Google Form */}
-        <h2
-          className="email-text"
-          onClick={() =>
-            window.open(
-              "https://docs.google.com/forms/d/e/1FAIpQLSew-QhFrSl-aIQDD5D4G4xrUn198eDMv_yEs5t-11l_kYk59Q/viewform?usp=publish-editor",
-              "_blank"
-            )
-          }
-        >
-          desikas209@gmail.com
-        </h2>
-
-        {/* Icons */}
-        <footer className="social-footer">
-          <a href="https://www.linkedin.com/in/desika-s-370b55363?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">
-            <i className="fa-brands fa-linkedin"></i>
+      {/* Main Content */}
+      <div className="contact-container">
+        <div className="contact-left">
+          <img src={contactImg} alt="Contact" className="contact-img" />
+        </div>
+        <div className="contact-right">
+          <h1>GET IN TOUCH</h1>
+          <p>Please contact me at this e-mail address to discuss your site creation needs.</p>
+          <a href={googleFormLink} target="_blank" rel="noreferrer" className="contact-email">
+            desikas209@gmail.com
           </a>
-
-          <a href="https://github.com/desikas2007" target="_blank">
-            <i className="fa-brands fa-github"></i>
-          </a>
-
-          <a href="https://www.instagram.com/_itz_desika_s/" target="_blank">
-            <i className="fa-brands fa-instagram"></i>
-          </a>
-        </footer>
-
-        {/* COPYRIGHT */}
-        <p className="copyright">
-          © 2025 Desika | All Rights Reserved
-        </p>
+        </div>
       </div>
-    </section>
+
+      <footer>
+        © 2025 Desika | All Rights Reserved
+      </footer>
+    </div>
   );
 }
